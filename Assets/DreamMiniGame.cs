83ac1e865a09ddd76b8ca287c1dffd7bf3a74980
kcrank98 <138240807+kcrank98@users.noreply.github.com>
2025-03-09 19:03:43 -0400

    DreamMiniGame
    ready for integration

--- Assets/DreamMiniGame.cs
+++ Assets/DreamMiniGame.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System;
 
 public class DreamMiniGame : MonoBehaviour
 {
@@ -10,40 +11,72 @@ public class DreamMiniGame : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float minX;
     [SerializeField] private float maxX;
+    [SerializeField] private float bonusPadding;
     private float target;
     private bool movingLeft;
+    private bool isKeyDown;
+    private bool tookDream;
     [SerializeField] private Transform arrow;
-    private RectTransform rectTransform;
+    [SerializeField] private Transform inBounds;
+    private RectTransform arrowRectTransform;
+    private RectTransform inBoundsRectTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         target = minX;
-        rectTransform = arrow.GetComponent<RectTransform>();
+        isKeyDown = false;
+        tookDream = false;
+        arrowRectTransform = arrow.GetComponent<RectTransform>();
+        inBoundsRectTransform = inBounds.GetComponent<RectTransform>();
+
+        float rtX = inBoundsRectTransform.sizeDelta.x;
+        inBoundsRectTransform.sizeDelta = new Vector2(rtX += bonusPadding, inBoundsRectTransform.sizeDelta.y);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isKeyDown = true;
+        }
         MoveCursor();
+    }
+
+    bool GetTookDream()
+    {
+        return tookDream;
     }
 
     void MoveCursor()
     {
-        
-        if (rectTransform.localPosition.x < minX )
+        if (!isKeyDown) 
         {
-            movingLeft = true;
-            target = maxX;
-        }
-        if (rectTransform.localPosition.x > maxX)
-        {
-            movingLeft = false;
+            if (arrowRectTransform.localPosition.x < minX)
+            {
+                movingLeft = true;
+                target = maxX;
+            }
+            if (arrowRectTransform.localPosition.x > maxX)
+            {
+                movingLeft = false;
 
-            target = minX;
+                target = minX;
+            }
+
+            arrowRectTransform.localPosition += new Vector3(movingLeft ? speed : -speed, 0, 0);
         }
-        
-        
-        rectTransform.localPosition += new Vector3( movingLeft ?  speed : -speed, 0, 0);
-        
+        else
+        {
+            if(arrowRectTransform.localPosition.x < (37f + (bonusPadding / 2)) && arrowRectTransform.localPosition.x > (-37f - (bonusPadding / 2)))
+            {
+                Debug.Log("Success!");
+                tookDream = true;
+            }
+            else
+            {
+                Debug.Log("Failure!");
+            }
+        }
     }
 }
