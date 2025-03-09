276bcdf445c5a34292c04718720aae8d5e31f1f9
Rowan Byington <Puddleglumstudio@gmail.com>
2025-03-09 18:19:56 -0400

    added dream counter

--- Assets/Scripts/DreamInvntory.cs
+++ Assets/Scripts/DreamInvntory.cs
@@ -6,7 +6,7 @@ public class DreamInvntory : MonoBehaviour
 {
     public static DreamInvntory instance;
     [SerializeField] private Slider fillBar;
-    [SerializeField] private TextMeshPro textObject;
+    [SerializeField] private TextMeshProUGUI textObject;
     public float dreamsNeeded;
     public float dreamBar;
     public bool endSequence;
